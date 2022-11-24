e6aa151b7cd0c9c45667520b481da4eebe27fbdc
BenoitSerrano <benoit.serrano10@gmail.com>
2022-11-24 16:02:00 +0100

    delete useless app.ts

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, Request, Response } from 'express';
-
-export { app };
-
-const app: Express = express();
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
