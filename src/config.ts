58e0bdea3c3a0148d76398826ebd9674e6d165d0
BenoitSerrano <benoit.serrano10@gmail.com>
2022-11-24 11:15:58 +0100

    move devDependencies to depenecies

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    SERVER_PORT: process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000,
+    SERVER_PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
     DATABASE_PORT: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 5432,
     DATABASE_HOST: process.env.DATABASE_HOST || '',
     DATABASE_NAME: process.env.DATABASE_NAME || '',
