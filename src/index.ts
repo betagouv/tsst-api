236c243c319c58df81f144f010a6c331bc4da23d
BenoitSerrano <benoit.serrano10@gmail.com>
2022-11-25 19:23:57 +0100

    export runApp from app

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
-import express, { Express } from 'express';
-import bodyParser from 'body-parser';
-
-import { router } from './router';
-import { config } from './config';
-import { dataSource } from './dataSource';
-
-async function runApp() {
-    const app: Express = express();
-
-    try {
-        await dataSource.initialize();
-    } catch (error) {
-        console.error(error);
-    }
-
-    app.use(bodyParser.json());
-
-    app.use('/api', router);
-
-    app.listen(config.SERVER_PORT, () => {
-        console.log(`Server is running at http://localhost:${config.SERVER_PORT}`);
-    });
-}
+import { runApp } from './app';
 
 runApp();
